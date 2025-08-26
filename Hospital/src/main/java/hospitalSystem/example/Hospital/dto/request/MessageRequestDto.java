86f0aee77714b7149486d0d8b53952cb17dab268
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-26 23:48:53 +0300

    Update MessageRequestDto.java

--- Hospital/src/main/java/hospitalSystem/example/Hospital/dto/request/MessageRequestDto.java
+++ Hospital/src/main/java/hospitalSystem/example/Hospital/dto/request/MessageRequestDto.java
@@ -1,4 +1,15 @@
 package hospitalSystem.example.Hospital.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class MessageRequestDto {
+    @NotNull(message = "Sender Id is required")
+    private Long senderId;
+    @NotNull(message = "Receiver Id is required")
+    private Long receiverId;
+    @NotBlank(message = "Content of message is required")
+    private String content;
 }
