22f7f4d1cc129c35d67733e5e6bfb678fbed90bd
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-26 23:52:13 +0300

    Update MessageRepository.java

--- Hospital/src/main/java/hospitalSystem/example/Hospital/repository/MessageRepository.java
+++ Hospital/src/main/java/hospitalSystem/example/Hospital/repository/MessageRepository.java
@@ -4,6 +4,14 @@ import hospitalSystem.example.Hospital.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
+    List<Message> findBySenderIdAndReceiverIdOrReceiverIdAndSenderId(
+            Long senderId, Long receiverId, Long receiverId2, Long senderId2);
+
+    List<Message> findByReceiverId(Long receiverId);
+
+    List<Message> findBySenderId(Long senderId);
 }
