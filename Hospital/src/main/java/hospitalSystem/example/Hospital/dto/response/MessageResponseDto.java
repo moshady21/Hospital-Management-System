060cf9e33f3b944f959ebb55a8c6ca76da6b4595
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-26 23:49:34 +0300

    Update MessageResponseDto.java

--- Hospital/src/main/java/hospitalSystem/example/Hospital/dto/response/MessageResponseDto.java
+++ Hospital/src/main/java/hospitalSystem/example/Hospital/dto/response/MessageResponseDto.java
@@ -1,4 +1,23 @@
 package hospitalSystem.example.Hospital.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageResponseDto {
+    private Long id;
+    private String content;
+    private LocalDateTime timestamp;
+
+    private Long senderId;
+    private String senderName;
+
+    private Long receiverId;
+    private String receiverName;
+
 }
