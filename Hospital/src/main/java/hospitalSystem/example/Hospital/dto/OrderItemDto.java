89f73715f4b9f0ed7f68c1f32ff0ed67eeba0194
Mohamed Shady <moshady21@gmail.com>
2025-08-28 00:28:33 +0300

    Update OrderItemDto.java

--- Hospital/src/main/java/hospitalSystem/example/Hospital/dto/OrderItemDto.java
+++ Hospital/src/main/java/hospitalSystem/example/Hospital/dto/OrderItemDto.java
@@ -1,4 +1,18 @@
 package hospitalSystem.example.Hospital.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class OrderItemDto {
+    private Long medicineId;
+    private String medicineName;
+    private int quantity;
+    private double price;
 }
+
+
